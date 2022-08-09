1d195faca65950b3874f14267e8e39be3c230a44
Will <swilson.carlson@hotmail.com>
2022-08-09 09:57:13 -0400

    commit tree d8acff7fe1e881f338ac52173967e2db2fd52056

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
 
+    # setter method
+    def name=(name)
+        @name = name
+    end
+
+    #getter method
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
+
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
 
+    #setter
+    def name=(name)
+        @name = name
+    end
+    #getter
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+    def job
+        @job
+    end
+
+
 end
